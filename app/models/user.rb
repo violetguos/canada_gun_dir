c225f65763da29a2dde778a781855e35fa36ec3d
Violet Guo <violetguos@users.noreply.github.com>
2021-12-05 15:20:49 -0500

    small UI change. enable text search for notes. ready for 1.0 w real data

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,11 @@ class User < ApplicationRecord
       # apparently you can't pass in the entire hash like this `users = User.where(params)` B/C sql injection
       raw_sql = ''
       params.each do |key, val|
-        raw_sql << key << ' = ' << "\'" << val << "\'" if val
+        if key != 'notes'
+          raw_sql << key << ' = ' << "\'" << val << "\'" if val
+        else
+          raw_sql << key << ' LIKE ' << "\'%" << val << "%\'" if val
+        end
         raw_sql << ' AND ' if key != params.keys.last
       end
       users = User.where(raw_sql)
