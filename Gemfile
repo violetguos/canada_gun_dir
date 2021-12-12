1724e52437a8e6cb3ce6c3e7c6faf3b1391216c9
Violet Guo <violetguos@users.noreply.github.com>
2021-12-12 17:21:29 -0500

    Mobile UI optim
    - add proper nav bar with event toggle for mobile
    - display limited table for mobile

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'simple_form', '~> 5.0', '>= 5.0.3'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
+gem 'rubocop', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- lib/excel_util.rb
+++ lib/excel_util.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
 module ExcelUtil
   def self.parse_notes_for_licnese_info(notes)
     # using a regex, serach for keywords "pal", "rpal"
-    licensed = false
-    licensed = true if notes.include? 'pal'
+    if notes.include? 'pal'
+      true
+    else
+      false
+    end
   end
 end
